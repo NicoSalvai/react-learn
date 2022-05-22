261f8bae88a461aa1d3b9bae71cd639971775359
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-22 14:46:32 -0300

    project3: using props to reuse Card component

--- project3-airbnb-clone/src/components/card/Card.js
+++ project3-airbnb-clone/src/components/card/Card.js
@@ -2,15 +2,15 @@ import './card.css'
 import img from '../../images/carrousel/carrousel1.png'
 import {FaStar} from 'react-icons/fa'
 
-export default function Card(){
+export default function Card(props){
     return (
         <div className="card">
-            <span className="card--tag">sold out</span>
-            <img src={img} className="card--img"></img>
+            <span className="card--tag">{props.status}</span>
+            <img src={props.img} className="card--img"></img>
             <div className="card--content">
-                <p><FaStar /> 5.0 <span className="gray--text">(6) - USA </span></p>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><b>From $136</b> / person</p>
+                <p><FaStar /> {props.stars} <span className="gray--text">{props.nationality} </span></p>
+                <p>{props.desc}</p>
+                <p><b>{props.price}</b> / person</p>
             </div>
         </div>
     )
--- project3-airbnb-clone/src/components/carousel/Carousel.js
+++ project3-airbnb-clone/src/components/carousel/Carousel.js
@@ -1,11 +1,36 @@
 import './carousel.css';
 import Card from '../card/Card';
+import img1 from '../../images/carrousel/carrousel1.png'
+import img2 from '../../images/carrousel/carrousel2.png'
+import img3 from '../../images/carrousel/carrousel3.png'
 
 export default function Carousel(){
     return (
         <div className="carousel">
-          <Card />
-          <Card />
+            <Card 
+                img={img1}
+                status="sold out"
+                stars="5.0"
+                nationality="(6) - USA "
+                desc="Life lessons with Katie Zaferes"
+                price="From $136"
+            />
+            <Card 
+                img={img2}
+                status="online"
+                stars="5.0"
+                nationality="(30) - USA "
+                desc="Learn wedding photography"
+                price="From $125"
+            />
+            <Card 
+                img={img3}
+                status="online"
+                stars="4.8"
+                nationality="(2) - USA "
+                desc="Group Mountain Biking"
+                price="From $50"
+            />
         </div>
     )
 }
