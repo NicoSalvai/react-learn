b2be0c1cd44589ac1f178cd8cfa730f1710c5490
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-22 19:08:43 -0300

    project3: improve badge behaviour

--- project3-airbnb-clone/src/components/card/Card.js
+++ project3-airbnb-clone/src/components/card/Card.js
@@ -3,9 +3,18 @@ import img from '../../images/carrousel/carrousel1.png'
 import {FaStar} from 'react-icons/fa'
 
 export default function Card(props){
+    
+    let badgeText ="";
+    if(props.openSpots === 0){
+        badgeText = "sold out";
+    }
+    else if(props.country === "Online"){
+        badgeText = "online";
+    }
+
     return (
         <div className="card">
-            { props.openSpots == 0 && <span className="card--tag">sold out</span>}
+            { badgeText && <span className="card--tag">{badgeText}</span>}
             <img src={props.img} className="card--img"></img>
             <div className="card--content">
                 <p><FaStar /> {props.stars} <span className="gray--text">({props.reviews}) - {props.country} </span></p>
