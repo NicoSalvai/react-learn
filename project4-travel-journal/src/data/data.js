4b01f02329b25a4ac3791de1142f5ac2e4e9a389
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-22 20:33:49 -0300

    project4: final fixes

--- project4-travel-journal/src/data/data.js
+++ project4-travel-journal/src/data/data.js
@@ -8,5 +8,23 @@ export default [
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Cairo",
+        location: "Egypt",
+        googleMapsUrl: "https://goo.gl/maps/8f2xzQUxHsZ2skFPA",
+        startDate: "24 Jan, 2021",
+        endDate: "31 Jan, 2021",
+        description: "As the British refused to leave their base around the Suez Canal, the Egyptian government cut off the water and refused to allow food into the Suez Canal base, announced a boycott ",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Egypt.Giza.Sphinx.02.jpg/300px-Egypt.Giza.Sphinx.02.jpg"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     }
 ]
