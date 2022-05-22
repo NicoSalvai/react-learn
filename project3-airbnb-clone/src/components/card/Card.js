b66c512a36138fc7a58fcd68a5e0f503d121d376
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-22 15:07:09 -0300

    project3: going back to non destructured props

--- project3-airbnb-clone/src/components/card/Card.js
+++ project3-airbnb-clone/src/components/card/Card.js
@@ -2,15 +2,15 @@ import './card.css'
 import img from '../../images/carrousel/carrousel1.png'
 import {FaStar} from 'react-icons/fa'
 
-export default function Card({status, img, stars, nationality, desc, price}){
+export default function Card(props){
     return (
         <div className="card">
-            <span className="card--tag">{status}</span>
-            <img src={img} className="card--img"></img>
+            <span className="card--tag">{props.status}</span>
+            <img src={props.img} className="card--img"></img>
             <div className="card--content">
-                <p><FaStar /> {stars} <span className="gray--text">{nationality} </span></p>
-                <p>{desc}</p>
-                <p><b>{price}</b> / person</p>
+                <p><FaStar /> {props.stars} <span className="gray--text">{props.nationality} </span></p>
+                <p>{props.desc}</p>
+                <p><b>{props.price}</b> / person</p>
             </div>
         </div>
     )
