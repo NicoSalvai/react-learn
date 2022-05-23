7d4e3ab3a946b3125d766ff3346d274ab861c65d
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-23 17:20:25 -0300

    project5: adding meme img with React state first iteration

--- project5-meme-generator/src/components/memeform/MemeForm.js
+++ project5-meme-generator/src/components/memeform/MemeForm.js
@@ -1,11 +1,21 @@
 import './memeform.css'
+import memeService from '../memes/memes'
+import React from 'react';
 
 export default function MemeForm() {
+    const [memeUrl, setMemeUrl] = React.useState()
+
+    function getNewMemeImage(){
+        let randomMemeIndex = parseInt(memeService.data.memes.length * Math.random());
+        setMemeUrl(memeService.data.memes[randomMemeIndex].url)
+    }
+
     return (
-        <form className="meme-form">
+        <div className="meme-form">
             <input type="text" id="first-text"/>
             <input type="text" id="second-text"/>
-            <button>Get a new meme image</button>
-        </form>
+            <button onClick={getNewMemeImage}>Get a new meme image</button>
+            <img src={memeUrl}/>
+        </div>
     )
 }
