0657551257ee5b4a9cd99a51665342da965bbd60
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-23 21:14:41 -0300

    project5: React useState with objects

--- project5-meme-generator/src/components/memeform/MemeForm.js
+++ project5-meme-generator/src/components/memeform/MemeForm.js
@@ -3,19 +3,26 @@ import memeService from '../memes/memes'
 import React from 'react';
 
 export default function MemeForm() {
-    const [memeUrl, setMemeUrl] = React.useState()
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        url: ""
+    })
 
     function getNewMemeImage(){
         let randomMemeIndex = parseInt(memeService.data.memes.length * Math.random());
-        setMemeUrl(memeService.data.memes[randomMemeIndex].url)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            url: memeService.data.memes[randomMemeIndex].url
+        }))
     }
 
     return (
         <div className="meme-form">
             <input type="text" id="first-text"/>
             <input type="text" id="second-text"/>
-            <button onClick={getNewMemeImage}>Get a new meme image</button>
-            <img src={memeUrl}/>
+            <button onClick={getNewMemeImage} className="meme-form-button">Get a new meme image</button>
+            <img src={meme.url}/>
         </div>
     )
 }
