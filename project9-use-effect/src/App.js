287a6a8ea9735fba1185a42155f5aa1f81785816
Nicolas Salvai <nicolas-salvai@hotmail.com>
2022-05-29 13:11:10 -0300

    useEffects tests

--- project9-use-effect/src/App.js
+++ project9-use-effect/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
+  const [starWarsData, setStarWarsData] = React.useState({})
+  const [count, setCount] = React.useState(0)
+  
+  // side effects
+  React.useEffect(() => console.log("Effect ran"), [count])
+
+  React.useEffect(() => {
+      console.log("Star wars ran")
+      fetch("https://swapi.dev/api/people/1")
+           .then(res => res.json())
+           .then(data => setStarWarsData(data))
+  }, [count])
+
+
   return (
     <div className="App">
+        
+        <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+
+        <h2>The count is {count}</h2>
+
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
 
     </div>
   );
 }
-
-export default App;
